c2f32d527ff01c68f1df96d63cfad67d8c7d259c
Led <ledest@gmail.com>
2023-08-18 07:22:04 +0300

    ssl_impersonate_app: add reload_modules/0

--- src/ssl_impersonate_app.erl
+++ src/ssl_impersonate_app.erl
@@ -10,8 +10,16 @@
 
 start(_StartType, _StartArgs) ->
     code:add_patha(filename:join([code:priv_dir(ssl_impersonate), "ssl", "ebin"])),
+    reload_modules(),
     supervisor:start_link(?MODULE, []).
 
 stop(_State) -> ok.
 
 init(_) -> {ok, {#{}, []}}.
+
+reload_modules() ->
+    lists:foreach(fun(F) ->
+                      M = list_to_atom(filename:basename(F, ".beam")),
+                      code:is_loaded(M) =/= false andalso code:load_file(M)
+                  end,
+                  filelib:wildcard(filename:join([code:priv_dir(ssl_impersonate), "src", "ebin", "*"]))).
