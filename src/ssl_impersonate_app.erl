7a328a640809e8ab25d477e9b5c02d81199c31cf
Led <ledest@gmail.com>
2023-08-18 08:12:24 +0300

    ssl_impersonate_app: improve start/2 and stop/1

--- src/ssl_impersonate_app.erl
+++ src/ssl_impersonate_app.erl
@@ -9,13 +9,23 @@
 -export([init/1]).
 
 start(_StartType, _StartArgs) ->
-    code:add_patha(filename:join([code:priv_dir(ssl_impersonate), "ssl", "ebin"])),
-    reload_modules(),
-    supervisor:start_link(?MODULE, []).
+    case code:get_mode() of
+        interactive ->
+            code:add_patha(path()),
+            reload_modules(),
+            supervisor:start_link(?MODULE, []);
+        M ->
+            ?LOG_CRITICAL("ssl_impersonate can work in interactive mode only, but node started in ~p mode", [M]),
+            {error, non_interactive}
+    end.
 
-stop(_State) -> ok.
+stop(_State) ->
+    code:del_path(path()),
+    reload_modules().
 
 init(_) -> {ok, {#{}, []}}.
+
+path() -> filename:join([code:priv_dir(ssl_impersonate), "ssl", "ebin"]).
 
 reload_modules() ->
     Ext = code:objfile_extension(),
